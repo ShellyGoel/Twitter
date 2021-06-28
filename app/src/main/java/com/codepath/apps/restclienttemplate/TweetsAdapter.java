19e0c950cf3cd841c1521d85fef60d0ab6453f71
Shelly Goel <shellygoel@fb.com>
2021-06-28 18:44:42 -0500

    Steps Completed so far: - Completed OAuth RestClient application   - downloaded template application for using a RestClient   - made network call to TwitterAPI to fetch home timeline for user when authenticated   - deserialized JSON data into User and Tweet objects and used RecyclerView to render each tweet (using Glide library to do remote image fetching and rendering).

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,84 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+//QUESTION: what does the extends part do again?
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+
+    Context context;
+    List<Tweet> tweets;
+
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    @NotNull
+    @Override
+
+
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view); 
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+    //get data at position
+        Tweet tweet = tweets.get(position);
+        //bind the tweet with view holder
+        holder.bind(tweet);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+    //pass in context and list of tweets
+    //for each row, inflate layout
+    //bind values based on the position of the element- Reccycleer view will tell us position to bind in
+
+    //define a viewholder
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvScreenName;
+
+        //itemView represents one row in the RecyclerView
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+
+
+        }
+
+        public void bind(Tweet tweet) {
+            tvBody.setText(tweet.body);
+            tvScreenName.setText(tweet.user.screenName);
+            Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
+        }
+    }
+
+
+}
